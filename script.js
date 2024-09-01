b865caf223098c3f2546f5bd611c62af4120ba3d
VishwasBJ <vishwasjai2004@gmail.com>
2024-09-01 23:58:53 +0530

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+const hie=document.getElementsByClassName("box");
+console.log(hie)
+
+// adding element
+let a=prompt("enter a here");
+document.write(a)
+let wite=confirm("do u want to write into the page");
+alert("vishwas is here");
+document.body.style.background = "yellow"
